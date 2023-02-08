95a5f41bccc370b899ecd2b83fe991f93c3c892e
stanvdkroon <stan.vanderkroon@student.uva.nl>
2023-02-08 14:38:08 +0100

    added implementations for add, delete, and get

--- python-flask-server-generated/swagger_server/controllers/default_controller.py
+++ python-flask-server-generated/swagger_server/controllers/default_controller.py
@@ -3,6 +3,7 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import *
 
 
 def add_student(body=None):  # noqa: E501
@@ -17,7 +18,8 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +32,9 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        return delete(student_id)
+    return 500, 'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +47,6 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        return get_by_id(student_id)
+    return 500, 'error'
